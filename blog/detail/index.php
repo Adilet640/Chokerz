46dc5b597a4e90fe6a5de6d9f78cdc7ed76aed85
Madi <adiletadil247@gmail.com>
2026-03-03 07:27:27 +0500

    Update index.php

--- blog/detail/index.php
+++ blog/detail/index.php
@@ -3,6 +3,10 @@
  * Детальная страница блога CHOKERZ
  * URL: /blog/#CODE#/
  * Символьный код берётся из переменной $CODE (настраивается в urlrewrite.php)
+ *
+ * Title и description устанавливаются внутри компонента custom:blog.detail
+ * через $APPLICATION->SetTitle() и $APPLICATION->SetPageProperty('description', ...)
+ * на основании полей элемента инфоблока (NAME и PREVIEW_TEXT / свойство SEO_DESCRIPTION).
  */
 
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
@@ -12,35 +16,26 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 use Bitrix\Main\Config\Option;
 
 $APPLICATION->SetPageProperty('body_class', 'page-blog-detail');
-?>
 
-<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php'); ?>
-
-<?php
 // Хлебные крошки
 $APPLICATION->IncludeComponent('bitrix:breadcrumb', '.default', [
-    'PATH'      => '',
-    'SITE_ID'   => SITE_ID,
-    'CACHE_TYPE'=> 'A',
-    'CACHE_TIME'=> 86400,
+    'PATH'       => '',
+    'SITE_ID'    => SITE_ID,
+    'CACHE_TYPE' => 'A',
+    'CACHE_TIME' => 86400,
 ]);
-?>
 
-<?php
 $APPLICATION->IncludeComponent(
     'custom:blog.detail',
     '.default',
     [
-        'IBLOCK_TYPE'    => 'blog',
-        'IBLOCK_ID'      => Option::get('chokerz', 'blog_iblock_id', 0),
-        'ELEMENT_CODE'   => $CODE ?? '',   // $CODE передаётся из urlrewrite.php
-        'RELATED_COUNT'  => 4,
-        'LIST_URL'       => '/blog/',
-        'CACHE_TYPE'     => 'A',
-        'CACHE_TIME'     => 3600,
+        'IBLOCK_TYPE'   => 'blog',
+        'IBLOCK_ID'     => Option::get('chokerz', 'blog_iblock_id', 0),
+        'ELEMENT_CODE'  => $CODE ?? '',   // $CODE передаётся из urlrewrite.php
+        'RELATED_COUNT' => 4,
+        'LIST_URL'      => '/blog/',
+        'CACHE_TYPE'    => 'A',
+        'CACHE_TIME'    => 3600,
     ],
     false
 );
-?>
-
-<?php require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
